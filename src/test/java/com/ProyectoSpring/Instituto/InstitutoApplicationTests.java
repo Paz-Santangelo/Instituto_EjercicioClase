79d208185a01a301986ee9cf4d910dd35002a1a8
Paz Santangelo <paz_santangelo@hotmail.com>
2026-05-19 15:17:35 -0300

    Creando proyecto para trabajar en clases.

---
+++ src/test/java/com/ProyectoSpring/Instituto/InstitutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoSpring.Instituto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstitutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
